84e21990c714cfa18d216d32655ba9d4fea4c04b
sinanefeozler <sinanefeozler88@gmail.com>
2025-07-07 16:24:07 -0700

    Updated by Github automation tool test

--- pi.py
+++ pi.py
@@ -1,14 +1,1 @@
 from random import random
-from math import pi
-
-n = int(input("Iteration number: "))
-
-circle = 0
-for i in range(n):
-    x,y = random(),random()
-    if x**2+y**2 <= 1:
-        circle += 1
-
-estimated = 4*circle/n
-print(f"Estimated value of pi: {estimated}")
-print(f"Error percentage: {abs(estimated-pi)*100/pi}")
